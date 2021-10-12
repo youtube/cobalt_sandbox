0d75346932c1d07086768bb16cc790139ef59242
Kaido Kert <kaidokert@google.com>
2021-10-12 14:42:56 -0700

    Import Cobalt 21.lts.6.305476

--- src/starboard/build/collect_deploy_content.py
+++ src/starboard/build/collect_deploy_content.py
@@ -12,16 +12,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Builds a symlink farm pointing to specified subdirectories of the input dir."""
+"""Builds a symlink farm pointing to specified subdirs of the input dir."""
 
 import argparse
 import logging
 import os
-import shutil
 import sys
 
 import _env  # pylint: disable=unused-import
-import starboard.tools.port_symlink as port_symlink
+from starboard.tools import port_symlink
 
 # The name of an environment variable that when set to |'1'|, signals to us that
 # we should log all output directories that we have populated.
@@ -38,21 +37,6 @@ def _ClearDir(path):
   if not os.path.exists(path):  # Works for symlinks for both *nix and Windows.
     return
   port_symlink.Rmtree(path)
-
-
-def _CopyTree(src_path, dst_path):
-  """
-  Copy tree with a safeguard for windows long path (>260).
-  On Windows Python is facing long path limitation, for more details see
-  https://bugs.python.org/issue27730
-  """
-  if os.sep == r'\\':
-    prefix = r'\\\\?\\'
-    if prefix not in src_path:
-      src_path = prefix + src_path
-    if prefix not in dst_path:
-      dst_path = prefix + dst_path
-  shutil.copytree(src_path, dst_path)
 
 
 def main(argv):
@@ -75,11 +59,6 @@ def main(argv):
       metavar='subdirs',
       nargs='*',
       help='subdirectories within both the input and output directories')
-  parser.add_argument(
-      '--copy_override',
-      action='store_true',
-      help='Overrides the behavior of collect_deploy_content to copy files, '
-      'instead of symlinking them.')
   options = parser.parse_args(argv[1:])
 
   if os.environ.get(_SHOULD_LOG_ENV_KEY, None) == '1':
@@ -128,9 +107,7 @@ def main(argv):
           msg += ' path points to an unknown type'
         logging.error(msg)
 
-    if options.copy_override:
-      _CopyTree(src_path, dst_path)
-    elif options.use_absolute_symlinks:
+    if options.use_absolute_symlinks:
       port_symlink.MakeSymLink(
           target_path=os.path.abspath(src_path),
           link_path=os.path.abspath(dst_path))
