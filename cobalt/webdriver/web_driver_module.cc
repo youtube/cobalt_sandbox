6e374b912cf4d8bd9c4f3a5c7053e4b5993b340d
Jelle Foks <jfoks@google.com>
2023-04-04 09:48:29 -0700

    Fix webdriver links. (#315)
    This updates webdriver links to their new locations, with versioning.
    
    b/200993308

--- cobalt/webdriver/web_driver_module.cc
+++ cobalt/webdriver/web_driver_module.cc
@@ -514,7 +514,7 @@ SessionDriver* WebDriverModule::GetSessionDriver(
   return NULL;
 }
 
-// https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol#status
+// https://www.selenium.dev/documentation/legacy/json_wire_protocol/#status
 void WebDriverModule::GetServerStatus(
     const base::Value* parameters,
     const WebDriverDispatcher::PathVariableMap* path_variables,
@@ -524,7 +524,7 @@ void WebDriverModule::GetServerStatus(
                              protocol::ServerStatus::ToValue(status_));
 }
 
-// https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol#sessions
+// https://www.selenium.dev/documentation/legacy/json_wire_protocol/#sessions
 void WebDriverModule::GetActiveSessions(
     const base::Value* parameters,
     const WebDriverDispatcher::PathVariableMap* path_variables,
@@ -538,7 +538,7 @@ void WebDriverModule::GetActiveSessions(
                              util::internal::ToValue(sessions));
 }
 
-// https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol#get-sessionsessionid
+// https://www.selenium.dev/documentation/legacy/json_wire_protocol/#sessionsessionid
 void WebDriverModule::CreateSession(
     const base::Value* parameters,
     const WebDriverDispatcher::PathVariableMap* path_variables,
@@ -564,7 +564,7 @@ void WebDriverModule::CreateSession(
                                  result_handler.get());
 }
 
-// https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol#delete-sessionsessionid
+// https://www.selenium.dev/documentation/legacy/json_wire_protocol/#sessions
 void WebDriverModule::DeleteSession(
     const base::Value* parameters,
     const WebDriverDispatcher::PathVariableMap* path_variables,
@@ -627,7 +627,7 @@ void WebDriverModule::StopScreencast(
   }
 }
 
-// https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol#sessionsessionidscreenshot
+// https://www.selenium.dev/documentation/legacy/json_wire_protocol/#sessionsessionidscreenshot
 void WebDriverModule::RequestScreenshot(
     const base::Value* parameters,
     const WebDriverDispatcher::PathVariableMap* path_variables,
