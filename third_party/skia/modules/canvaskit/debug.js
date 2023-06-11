b1ab7a92cccb49d208c5d7ef0954432185760105
Kaido Kert <kaidokert@google.com>
2023-06-10 17:06:24 -0700

    Delete everything

--- third_party/skia/modules/canvaskit/debug.js
+++
@@ -1,3 +0,0 @@
-function SkDebug(msg) {
-  console.warn(msg);
-}
--- third_party/skia/modules/canvaskit/release.js
+++
@@ -1,4 +0,0 @@
-function SkDebug(msg) {
-  // by leaving this blank, closure optimizes out calls (and the messages)
-  // which trims down code size and marginally improves runtime speed.
-}
