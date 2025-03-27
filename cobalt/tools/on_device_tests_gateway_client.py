e3e92c9dfc43fd2b9fd853e2a879368466ad53a7
Felipe Javier Mira Bocca <fmira@google.com>
2025-03-27 15:20:43 -0700

    Update on_device_tests_gateway_client.py

--- cobalt/tools/on_device_tests_gateway_client.py
+++ cobalt/tools/on_device_tests_gateway_client.py
@@ -28,7 +28,7 @@ import on_device_tests_gateway_pb2_grpc
 
 _WORK_DIR = '/on_device_tests_gateway'
 
-# For local testing, set: _ON_DEVICE_TESTS_GATEWAY_SERVICE_HOST = ('localhost')
+# For local testing , set: _ON_DEVICE_TESTS_GATEWAY_SERVICE_HOST = ('localhost')
 _ON_DEVICE_TESTS_GATEWAY_SERVICE_HOST = (
     'on-device-tests-gateway-service.on-device-tests.svc.cluster.local')
 _ON_DEVICE_TESTS_GATEWAY_SERVICE_PORT = '50052'
