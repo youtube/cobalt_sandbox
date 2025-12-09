0939e434565799b6c290997f6be01749f12f768f
Felipe Mira <fmira@google.com>
2025-12-09 19:39:31 +0000

    Fix lint issues in gateway client

--- cobalt/tools/on_device_tests_gateway_client.py
+++ cobalt/tools/on_device_tests_gateway_client.py
@@ -243,7 +243,7 @@ def _process_test_requests(args: argparse.Namespace) -> List[Dict[str, Any]]:
       files = _unit_test_files(args, target_name)
       params = _unit_test_params(args, target_name, dir_on_device)
 
-    elif args.test_type == 'e2e_test' or args.test_type == 'yts_test':
+    elif args.test_type in ('e2e_test', 'yts_test'):
       test_target = target_data['target']
       test_attempts = target_data.get('test_attempts', '')
       if test_attempts:
@@ -409,7 +409,7 @@ def main() -> int:
   args = parser.parse_args()
 
   # TODO(b/428961033): Let argparse handle these checks as required arguments.
-  if args.test_type == 'e2e_test' or args.test_type == 'yts_test':
+  if args.test_type in ('e2e_test', 'yts_test'):
     if not args.cobalt_path:
       raise ValueError('--cobalt_path is required for e2e_test or yts_test')
   elif args.test_type == 'unit_test':
